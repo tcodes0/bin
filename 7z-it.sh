0102ededef2a132e4b15eb2515f2c322781b8efe
Raphael Thomazella <1357rtr@gmail.com>
2017-12-24 02:01:50 -0200

    tried improving but ended up more or less the same

--- 7z-it.sh
+++ 7z-it.sh
@@ -1,8 +1,6 @@
-#! /bin/bash
-
+# ! /bin/bash
 if [ $# == "0" ]; then
     echo "7z-it.sh needs at least 1 arg"
     exit 1
 fi
-
 tar cf - "$1" 1>/dev/null 2>1 | 7za a -si -mx=7 "$1".tar.7z 1>/dev/null
