be9da63ccc2406b741b093d952501728f16854db
Raphael Thomazella <1357rtr@gmail.com>
2017-12-24 00:48:58 -0200

    added touch-big

---
+++ touch-big.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+help-text(){
+  echo "Example: touch-big.sh 200"
+  echo "creates a file with 200 MB on the current dir called 'big'"
+}
+if [[ "$#" != 0 ]]; then
+  dd if=/dev/zero bs=1000 count=$(($1*1000)) > ./big
+  exit 0
+else
+  filezise=200
+  help-text
+  exit 1
+fi
