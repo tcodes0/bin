c11d02d278a560ecacffcda83ff1a9decaa9f340
Raphael Thomazella <1357rtr@gmail.com>
2018-02-19 21:11:21 -0300

    webbot.sh - added more .sass files, now handles multiples instances

--- webbot.sh
+++ webbot.sh
@@ -1,3 +1,11 @@
-#! /bin/bash
-sass --watch css/index.sass:css/index.css 2>/dev/null 1>&2 &
-reload
+#! /usr/bin/env bash
+instances=$(echo $(ps | grep $HOME/bin/webbot.sh | wc -l))
+instances=$(( $instances - 4))
+port=8080
+if [[ "$instances" -gt 0 ]]; then
+  port=$((port + $instances))
+fi
+sass --watch css/index.sass:css/index.css   2>/dev/null 1>&2 &
+sass --watch css/grid.sass:css/grid.css     2>/dev/null 1>&2 &
+sass --watch css/boiler.sass:css/boiler.css 2>/dev/null 1>&2 &
+reload -p $port
