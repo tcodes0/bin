09c348fce0d9cbf3fbdf060b16086bae052fa2ad
Raphael Thomazella <1357rtr@gmail.com>
2018-02-28 16:53:02 -0300

    tere.sh - now <3 goes into clipboard

--- tere.sh
+++ tere.sh
@@ -6,6 +6,4 @@ fi
 model="💕💕💕💕💕💕💕💕💕💕💕\n💕🌻🌻🌻💕💕💕🌻🌻🌻💕\n🌻🌻🌻🌻🌻💕🌻🌻🌻🌻🌻\n🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻\n💕🌻🌻🌻🌻🌻🌻🌻🌻🌻💕\n💕💕🌻🌻🌻🌻🌻🌻🌻💕💕\n💕💕💕🌻🌻🌻🌻🌻💕💕💕\n💕💕💕💕🌻🌻🌻💕💕💕💕\n💕💕💕💕💕🌻💕💕💕💕💕\n"
 model=$(gsed -Ee s/💕/"$1"/gm <<< "$model")
 model=$(gsed -Ee s/🌻/"$2"/gm <<< "$model")
-echo -e $model > ./tereout.txt
-Open ./tereout.txt
-rm ./tereout.txt
+echo -e $model | pbcopy
