21320b3a1d933727590adef3cfe446dfb0ca108b
Raphael Thomazella <1357rtr@gmail.com>
2018-03-31 21:44:14 -0300

    changed model to a rare emoticon.

--- tere.sh
+++ tere.sh
@@ -3,7 +3,7 @@ if [[ "$#" == 0 ]] || [ "$1" == '-h' ]; then
   echo provide two emoticons to make a ❤️   for her.
   exit
 fi
-model="💕💕💕💕💕💕💕💕💕💕💕\n💕🌻🌻🌻💕💕💕🌻🌻🌻💕\n🌻🌻🌻🌻🌻💕🌻🌻🌻🌻🌻\n🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻\n💕🌻🌻🌻🌻🌻🌻🌻🌻🌻💕\n💕💕🌻🌻🌻🌻🌻🌻🌻💕💕\n💕💕💕🌻🌻🌻🌻🌻💕💕💕\n💕💕💕💕🌻🌻🌻💕💕💕💕\n💕💕💕💕💕🌻💕💕💕💕💕\n"
-model=$(gsed -Ee s/💕/"$1"/gm <<< "$model")
-model=$(gsed -Ee s/🌻/"$2"/gm <<< "$model")
+model="🕞🕞🕞🕞🕞🕞🕞🕞🕞🕞🕞\n🕞🕛🕛🕛🕞🕞🕞🕛🕛🕛🕞\n🕛🕛🕛🕛🕛🕞🕛🕛🕛🕛🕛\n🕛🕛🕛🕛🕛🕛🕛🕛🕛🕛🕛\n🕞🕛🕛🕛🕛🕛🕛🕛🕛🕛🕞\n🕞🕞🕛🕛🕛🕛🕛🕛🕛🕞🕞\n🕞🕞🕞🕛🕛🕛🕛🕛🕞🕞🕞\n🕞🕞🕞🕞🕛🕛🕛🕞🕞🕞🕞\n🕞🕞🕞🕞🕞🕛🕞🕞🕞🕞🕞\n"
+model=$(gsed -Ee s/🕞/"$1"/gm <<< "$model")
+model=$(gsed -Ee s/🕛/"$2"/gm <<< "$model")
 echo -e $model | pbcopy
