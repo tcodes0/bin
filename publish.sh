8658332d79510d95cf1e19e84b8552919c2e5c55
Raphael Thomazella <1357rtr@gmail.com>
2018-03-31 21:43:51 -0300

    added help, overall improvements and better messages

--- publish.sh
+++ publish.sh
@@ -1,4 +1,14 @@
 #! /usr/bin/env bash
+if [[ "$@" =~ -h ]]; then
+  precho this utility:
+  precho "post processes css using >post-css"
+  precho "transpiles JS with >npm run build"
+  precho "moves files to ./public"
+  precho "kills sourceMappingURLs on css files"
+  precho "and optionally calls publish-rsync, which:"
+  publish-rsync -h
+  exit
+fi
 if [[ ! "$(dirname $PWD)" =~ ^/Users/vamac/Code ]]; then   # Don't run outside ~/Code
   bailout "Can't run publish outside $(echo ~/Code)"
 fi
@@ -9,15 +19,14 @@ if [ ! -d ${PWD}/public ]; then
   echoform 1 49 33 "♦︎ No ./public dir found, creating it for you..."
   mkdir ./public || bailout "Failed to make dir ./public"
 fi
-post-css
-echoform 1 49 32 "✔ Moving project files to ./public"
-\cp -R ./*.html ./public # the backlash \ on \cp avoids any aliases called "cp"
-\cp -R ./js ./public
+post-css && echoform 1 49 32 "✔ Post-processed CSS"
+npm run build 1>/dev/null && echoform 1 49 32 "✔ Transpiled JS"
+(\cp -R ./*.html ./public # the backlash \ on \cp avoids any aliases called "cp"
+# \cp -R ./js ./public
 \cp -R ./css/img ./public/css
 \cp -R ./LICENSE* ./public
-\cp -R ./*json ./public
-echoform 1 49 32 "✔ Killing sourcemapping lines in css files on ./public/css/"
+\cp -R ./*json ./public) && echoform 1 49 32 "✔ Moved project files to ./public"
 for file in ./public/css/*.css; do
   gsed --in-place --regexp-extended --expression='/[/][*]# sourceMappingURL.*[*][/]/d' $file
-done
-publish-rsync
+done && echoform 1 49 32 "✔ Killed sourcemapping lines in css files on ./public/css/"
+publish-rsync "$@"
