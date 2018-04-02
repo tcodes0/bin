541d37b3d528564856b4ae1a6b5a41458464a679
Raphael Thomazella <1357rtr@gmail.com>
2018-04-02 00:23:40 -0300

    removed old version from color.sh, changed publish to use new color

--- color.sh
+++ color.sh
@@ -1,31 +1,11 @@
-old() {
-  if [ "$1" == "-h" -o "$1" == "--help" ] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
-    echo "usage: color 1 49 39 formatted text"
-    echo -n "result: "; color 1 49 39 "formatted text"
-    echo "the formatting is reset after each call, or use --clear/-c"
-    exit
-  fi
-
-  if [ "$1" == "-c" -o "$1" == "--clear" ]; then
-    shift
-    echo -ne '\e[0m'
-    exit
-  fi
-
-  local i=0
-  while [[ "$1" =~ ^[0-9]+$ ]] && [ $i -lt 3 ]; do #regex to match numbers && 3 numbers max
-    echo -ne "\e[$1m"
-    shift
-    i=$((i+1))
-  done
-
-  #echos all remaining arguments (words), and then formating is reset.
-  echo -e "$@\e[0m"
-}
-
-new() {
   source $HOME/bin/optar.sh || bailout "optar.sh not found."
   parse-options "$@"
+
+  # for debuging
+  if [[ "$1" == "-x" ]]; then
+    set -x
+    shift
+  fi
 
   if [[ -n $h ]] || [[ -n $help ]]; then
     precho "example: color --yellow --bold foobar \
@@ -51,6 +31,20 @@ new() {
     exit
   fi
 
+  # only bold or white and bold prints yellow. Implementation quirk.
+  if [[ -n $bold ]]; then
+    printf "\e[1m"
+  elif [[ -n $dim ]]; then
+    printf "\e[2m"
+  elif [[ -n $underline ]]; then
+    printf "\e[4m"
+  elif [[ -n $blink ]]; then
+    printf "\e[5m"
+  elif [[ -n $box ]]; then
+    printf "\e[7m"
+  fi
+
+  # foregrounds
   if [[ -n $black ]]; then
     printf "\e[30m"
   elif [[ -n $red ]]; then
@@ -87,19 +81,7 @@ new() {
     printf "\e[97m"
   fi
 
-  # only bold or white and bold prints yellow. Implementation quirk.
-  if [[ -n $bold ]]; then
-    printf "\e[1m"
-  elif [[ -n $dim ]]; then
-    printf "\e[2m"
-  elif [[ -n $underline ]]; then
-    printf "\e[4m"
-  elif [[ -n $blink ]]; then
-    printf "\e[5m"
-  elif [[ -n $box ]]; then
-    printf "\e[7m"
-  fi
-
+  # non-crazy backgrounds
   case $bg in
     "black")
       printf "\e[40m"
@@ -115,18 +97,7 @@ new() {
     ;;
   esac
 
+  # print all arguments
   printf "$(get-arguments)"
+  # end formatting
   printf "\e[0m\n"
-}
-
-if [[ "$1" == "-x" ]]; then
-  set -x
-  shift
-fi
-
-if [[ "$1" == "--dev" ]]; then
-  shift
-  new "$@"
-else
-  old "$@"
-fi
--- publish.sh
+++ publish.sh
@@ -12,21 +12,18 @@ fi
 if [[ ! "$(dirname $PWD)" =~ ^/Users/vamac/Code ]]; then   # Don't run outside ~/Code
   bailout "Can't run publish outside $(echo ~/Code)"
 fi
-# if [[ ! -f "$(echo css/*css)" ]] || [[ ! -f "$(echo js/*js)" ]] || [[ ! -f "$(echo *html)" ]]; then
-#   bailout "$PWD - need CSS, JS and HTML files to work with."
-# fi
 if [ ! -d ${PWD}/public ]; then
-  color 1 49 33 "♦︎ No ./public dir found, creating it for you..."
+  color --bold --yellow "♦︎ No ./public dir found, creating it for you..."
   mkdir ./public || bailout "Failed to make dir ./public"
 fi
-post-css && color 1 49 32 "✔ Post-processed CSS"
-npm run build 1>/dev/null && color 1 49 32 "✔ Transpiled JS"
+post-css && color --bold --green "✔ Post-processed CSS"
+npm run build 1>/dev/null && color --bold --green "✔ Transpiled JS"
 (\cp -R ./*.html ./public # the backlash \ on \cp avoids any aliases called "cp"
 # \cp -R ./js ./public
 \cp -R ./css/img ./public/css
 \cp -R ./LICENSE* ./public
-\cp -R ./*json ./public) && color 1 49 32 "✔ Moved project files to ./public"
+\cp -R ./*json ./public) && color --bold --green "✔ Moved project files to ./public"
 for file in ./public/css/*.css; do
   gsed --in-place --regexp-extended --expression='/[/][*]# sourceMappingURL.*[*][/]/d' $file
-done && color 1 49 32 "✔ Killed sourcemapping lines in css files on ./public/css/"
+done && color --bold --green "✔ Killed sourcemapping lines in css files on ./public/css/"
 publish-rsync "$@"
