4ded4e51bbcfcd4afa8dca2bcc52d87388fb5241
Raphael Thomazella <1357rtr@gmail.com>
2018-04-04 14:06:54 -0300

    refactored everything and renamed a bunch of stuff.

--- bkp.sh
+++ bkp.sh
@@ -1,38 +1,37 @@
 #! /usr/bin/env bash
-##-----------------------  Deps  ----------------------##
-if [[ -f $HOME/bin/bkp-vars-and-routines.sh ]]; then
-  source $HOME/bin/bkp-vars-and-routines.sh
-else
-  echo $HOME/bin/bkp-vars-and-routines.sh not found. Exiting...
-  exit 1
-fi
+##-----------------------  Deps & Setup  ----------------------##
+source "$HOME/bin/bkp-vars-and-routines.sh" || bailout "Dependency failed"
+source "$HOME/bin/optar.sh" || bailout "Dependency failed"
+source "$HOME/bin/progress.sh" || bailout "Dependency failed"
+source "$HOME/bin/paralol.sh" || bailout "Dependency failed"
+
+parse-options "$@"
+maybeDebug
+
 ######----------------- Quick exits  -----------------######
 if [[ "$#" != 0 ]]; then
-  case "$1" in
-    --print | -p)
-    print
+  if [ "$print" -o "$p" ]; then
+    do-print
     exit 0
-    ;;
-    *)
-    help
+  elif [ "$help" -o "$h" ]; then
+    do-help
     exit 1
-    ;;
-  esac
+  fi
 fi
+
 #-- Test for backup drive plugged in
-if ! [ -d "$BKPDIR" ]; then
-  precho -r "Seagate is not plugged in! Aborting"
-  exit 1
-fi
+[ -d "$BKPDIR" ] || bailout "Backup destination not plugged in?"
+
 ######----------------- Main  -----------------######
 now-running
 pathlist
-app-list
-zip-move
-do-homebrew
-do-mackup
+safelist
+dellist
+applist
+ziplist
+do-software
 do-brewfile
-#-- progress
+do-mackup
+
 progress total ~/.bkp-run-times
-#-- Scheduler
 scheduler.sh --record
