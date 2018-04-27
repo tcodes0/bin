5fb35db7b46dd12eba0d6f7e99bd92a3621cbc52
Raphael Thomazella <1357rtr@gmail.com>
2018-04-27 16:50:54 -0300

    publi.sh -  updated and expanded to work with gulp, improv help text

--- publish.sh
+++ publish.sh
@@ -5,15 +5,20 @@ parse-options "$@"
 maybeDebug
 
 if [[ "$h" ]]; then
-  precho "this utility:\n\
-  post processes css using post-css\n\
-  transpiles JS with npm run build\n\
-  moves files to ./public\n\
-  kills sourceMappingURLs on css files\n\
-  options (passed to upload-rsync too):\n\
-  --dry-run\n\
-  and calls publish-rsync, which:"
-  printf "\n"
+  precho "publish.sh:
+  \n post processes css using post-css
+  \n transpiles JS with npx gulp
+  \n moves misc files to ./build
+  \n kills sourceMappingURLs on css files.
+  \n
+  \n options (passed to upload-rsync too):
+  \n
+  \n -h         \t\t see this help
+  \n --rebuild-imgs \t regenerate all image assets
+  \n --purge      \t delete ./build
+  \n --dry-run
+  \n and calls upload-rsync, which:
+  \n"
   upload-rsync.sh -h
   exit
 fi
@@ -26,20 +31,30 @@ case " " in
   "$dry_run")
     DRY="echo"
   ;;
+  "$purge")
+    $DRY npx gulp purgeBuild && precho -k "./build deleted"
+    exit
+  ;;
 esac
 
-if [ ! -d ${PWD}/public ]; then
-  precho -w "No ./public dir found, creating it for you..."
-  $DRY mkdir ./public || bailout "Failed to make dir ./public"
+if [ ! -d ${PWD}/build ]; then
+  precho -w "No ./build dir found, creating it"
+  $DRY mkdir ./build || bailout "Failed to make dir ./build"
 fi
+
 $DRY post-css && precho -k "Post-processed CSS"
-$DRY npm run build 1>/dev/null && precho -k "Transpiled JS"
-($DRY \cp -R ./*.html ./public # the backlash \ on \cp avoids any aliases called "cp"
-# $DRY \cp -R ./js ./public
-$DRY \cp -R ./css/img ./public/css
-$DRY \cp -R ./LICENSE* ./public
-$DRY \cp -R ./*json ./public) && precho -k "Moved project files to ./public"
-for file in ./public/css/*.css; do
+$DRY npx gulp scripts 1>/dev/null 2>&1 && precho -k "Transpiled JS"
+($DRY \cp -R ./*html ./build # the backlash \ on \cp avoids any aliases called "cp"
+$DRY \cp -R ./*php ./build
+# $DRY \cp -R ./css/img ./build/css
+$DRY \cp -R ./LICENSE* ./build
+$DRY \cp -R ./*json ./build) && precho -k "Moved project files to ./build"
+for file in ./build/css/*.css; do
   $DRY gsed --in-place --regexp-extended --expression='/[/][*]# sourceMappingURL.*[*][/]/d' $file
-done && precho -k "Killed sourcemapping lines in css files on ./public/css/"
+done && precho -k "Killed sourcemapping lines in css files on ./build/css/"
+
+if [[ "$rebuild_imgs" ]]; then
+  precho -w "Regenerating all build images..."
+  $DRY npx gulp optimizeImgs && precho -k "Images regenerated"
+fi
 upload-rsync.sh "$@"
