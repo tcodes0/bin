103ebeb49dc250ed4b958fe0447891e9c6f2a083
Raphael Thomazella <1357rtr@gmail.com>
2018-04-27 16:52:40 -0300

    upload-rsync - update to work with gulp, improv help message, renames

--- upload-rsync.sh
+++ upload-rsync.sh
@@ -5,12 +5,12 @@ parse-options "$@"
 maybeDebug
 
 if [[ "$h" ]]; then
-  precho "upload-rsync.sh uploads ./public via ssh to web server\n\
-    -y\t\t auto confirm\n\
-    -v\t\t verbose rsync\n\
-    -h\t\t see this help\n\
-    --delete\t use rsync --delete\n\
-    --dry-run"
+  precho "upload-rsync.sh uploads ./build via ssh to web server
+  \n -y     \t\t auto confirm
+  \n -v     \t\t verbose rsync
+  \n -h     \t\t see this help
+  \n --clean \t remove extra files on server
+  \n --dry-run"
   exit
 fi
 
@@ -35,7 +35,7 @@ fi
 if [ "$REPLY" == "y" ] || [ "$REPLY" == "yes" ] || [ "$REPLY" == "Y" ] || [ "$REPLY" == "YES" ] || [ "$REPLY" == "" ]; then
   echo -ne "\e[1;49;33m♦︎ Uploading all files with rsync...\e[0m"
   options="--recursive --update --inplace --no-relative --checksum --compress"
-  if [[ "$delete" ]]; then
+  if [[ "$clean" ]]; then
     options=$options" --delete"
   fi
   if [ "$v" -o "$verbose" ]; then
@@ -44,7 +44,7 @@ if [ "$REPLY" == "y" ] || [ "$REPLY" == "yes" ] || [ "$REPLY" == "Y" ] || [ "$RE
   SSH="ssh -p 21098"
   host="tazemuad@server179.web-hosting.com:/home/tazemuad"
   remote_dir=$host/sites/$(basename $PWD)/
-  local_dir=$PWD/public/
+  local_dir=$PWD/build/
   $DRY rsync $options -e "$SSH" $local_dir $remote_dir
   if [[ "$?" == 0 ]]; then
     echo -e "\r\e[1;49;32m✔ Done $(date +%H:%M)                                         \e[0m"
