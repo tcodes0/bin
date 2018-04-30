62d2de6116bc7823b2a6237eba963d548e223cb1
Raphael Thomazella <1357rtr@gmail.com>
2018-04-30 19:36:17 -0300

    added update to caniuse, formatting opts

--- software-update.sh
+++ software-update.sh
@@ -6,16 +6,20 @@ work(){
     (brew upgrade 1>/dev/null 2>&1
     brew cask upgrade 1>/dev/null)
     progress finish "$?"
+
   progress start "Scrubbing homebrew's cache"
     (brew cleanup -s --prune=31 1>/dev/null
     brew cask cleanup 1>/dev/null)
     progress finish "$?"
+
   progress start "Updating NPM global packages"
-    npm update -g 1>/dev/null 2>&1
+    (npm update -g 1>/dev/null 2>&1
+    npm install -g caniuse)
     progress finish "$?"
+
   progress start "Updating gems"
-  # /usr/bin/gem often conflicts. MacOS version is root - wheel, no write perm.
-  /usr/local/bin/gem update 1>/dev/null
+    # /usr/bin/gem often conflicts. MacOS version is root - wheel, no write perm.
+    /usr/local/bin/gem update 1>/dev/null
     progress finish "$?"
 }
 
@@ -25,13 +29,16 @@ case "$1" in
   exit 0
   ;;
 esac
+
 #confirm upgrade
 precho "Upgrade all cli-software now? (y/n)
 \n ...defaulting to no in 5s"
 read -t 5
+
 if [ "$?" != 0 ]; then
   exit 1
 fi
+
 if [ "$REPLY" == "y" ] || [ "$REPLY" == "yes" ] || [ "$REPLY" == "Y" ] || [ "$REPLY" == "YES" ]; then
   work
 fi
