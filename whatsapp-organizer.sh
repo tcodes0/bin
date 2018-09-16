23b46b22f880a65fc0f20c7665f65c1da5679602
Raphael Thomazella <thomazella9@gmail.com>
2018-09-16 13:40:26 -0300

    shcheck shfmt whatsapp-organizer

--- whatsapp-organizer.sh
+++ whatsapp-organizer.sh
@@ -2,19 +2,19 @@
 # - - -- - - - - - - - - - - - - - - - - - - - - - - -  Vars
 months="01 02 03 04 05 06 07 08 09 10 11 12"
 # - - -- - - - - - - - - - - - - - - - - - - - - - - -  Functions
-findYearsWithFiles () {
+findYearsWithFiles() {
   #1 a dir to search on
   #2 $pat global
   #returns a list of years
   local where=$1
-  local years=$(echo {2000..2099})
+  local years && years=$(echo {2000..2099})
   local results=''
   for year in $years; do
     if [[ $(gfind "$where" -maxdepth 1 -name "${pat}${year}*") != '' ]]; then
       results=$results" "$year
     fi
   done
-  echo -n $results
+  echo -n "$results"
 }
 moveFiles() {
   #1 a dir to search on
@@ -22,48 +22,48 @@ moveFiles() {
   #3 $pat global
   #4 $months global
   #sideffect moves files
-  if [ $dry ]; then echo -e "\n\n\n\n--------------------------- MOVING -------------------------\n\n\n\n" ; fi
+  if [ "$dry" ]; then echo -e "\\n\\n\\n\\n--------------------------- MOVING -------------------------\\n\\n\\n\\n"; fi
   local where=$1
   shift
-  local years=$@
+  local years=$*
   local files=''
   local destination=''
   local fileType=''
   for year in $years; do
     gfind "$where" -maxdepth 1 -name "${pat}${year}*" -print0 | rename -0 --sanitize
-      for month in $months; do
-        files=$(gfind "$where" -maxdepth 1 -name "${pat}${year}${month}*")
-        if [[ $files != '' ]]; then
-          if [[ $where =~ Video ]]; then
-            fileType=zap-vids
-          else
-            [ ! -n "$manual" ] && fileType=zap
-            [ -n "$manual" ] && fileType="$manualFileType"
-          fi
-          if [[ $where =~ Sent ]]; then
-            destination="$(dirname $dir)/$year/$month/$fileType/sent"
-          else
-            destination="$dir/$year/$month/$fileType"
-          fi
-          if [[ ! -d $destination ]]; then
-            $dry mkdir -p $destination || bailout
-          fi
-          for file in $files; do
-            $dry mv "$file" $destination
-          done
+    for month in $months; do
+      files=$(gfind "$where" -maxdepth 1 -name "${pat}${year}${month}*")
+      if [[ $files != '' ]]; then
+        if [[ $where =~ Video ]]; then
+          fileType=zap-vids
+        else
+          [ ! -n "$manual" ] && fileType=zap
+          [ -n "$manual" ] && fileType="$manualFileType"
         fi
-        files=''
-      done
+        if [[ $where =~ Sent ]]; then
+          destination="$(dirname "$dir")/$year/$month/$fileType/sent"
+        else
+          destination="$dir/$year/$month/$fileType"
+        fi
+        if [[ ! -d $destination ]]; then
+          $dry mkdir -p "$destination" || bailout
+        fi
+        for file in $files; do
+          $dry mv "$file" "$destination"
+        done
+      fi
+      files=''
+    done
   done
 }
-find_whatsappDir () {
+find_whatsappDir() {
   if [[ -d 'WhatsApp Images' ]] && [ "$pat" == "IMG-" ]; then
-     mv WhatsApp\ Images WhatsApp_Images || bailout
+    mv WhatsApp\ Images WhatsApp_Images || bailout
     dir=./WhatsApp_Images
   elif [[ -d 'WhatsApp_Images' ]] && [ "$pat" == "IMG-" ]; then
     dir=./WhatsApp_Images
   elif [[ -d 'WhatsApp Video' ]] && [ "$pat" == "VID-" ]; then
-     mv WhatsApp\ Video WhatsApp_Video || bailout
+    mv WhatsApp\ Video WhatsApp_Video || bailout
     dir=./WhatsApp_Video
   elif [[ -d 'WhatsApp_Video' ]] && [ "$pat" == "VID-" ]; then
     dir=./WhatsApp_Video
@@ -72,57 +72,57 @@ find_whatsappDir () {
     exit 1
   fi
 }
-renameFolders (){
-  if [ $dry ]; then echo -e "\n\n\n\n--------------------------- RENAMING -------------------------\n\n\n\n" ; fi
+renameFolders() {
+  if [ "$dry" ]; then echo -e "\\n\\n\\n\\n--------------------------- RENAMING -------------------------\\n\\n\\n\\n"; fi
   #1 dir global
-  local folders=$(gfind "$dir" -regextype posix-extended -regex '.*[/][0-9][0-9]')
+  local folders && folders=$(gfind "$dir" -regextype posix-extended -regex '.*[/][0-9][0-9]')
   if [[ "$folders" == '' ]]; then
     echo "no hits for renaming"
   fi
   for folder in $folders; do
     case $folder in
-      *01)
-        $dry mv $folder ${folder}jan
+    *01)
+      $dry mv "$folder" "${folder}jan"
       ;;
-      *02)
-        $dry mv $folder ${folder}feb
+    *02)
+      $dry mv "$folder" "${folder}feb"
       ;;
-      *03)
-        $dry mv $folder ${folder}mar
+    *03)
+      $dry mv "$folder" "${folder}mar"
       ;;
-      *04)
-        $dry mv $folder ${folder}apr
+    *04)
+      $dry mv "$folder" "${folder}apr"
       ;;
-      *05)
-        $dry mv $folder ${folder}may
+    *05)
+      $dry mv "$folder" "${folder}may"
       ;;
-      *06)
-        $dry mv $folder ${folder}jun
+    *06)
+      $dry mv "$folder" "${folder}jun"
       ;;
-      *07)
-        $dry mv $folder ${folder}jul
+    *07)
+      $dry mv "$folder" "${folder}jul"
       ;;
-      *08)
-        $dry mv $folder ${folder}aug
+    *08)
+      $dry mv "$folder" "${folder}aug"
       ;;
-      *09)
-        $dry mv $folder ${folder}sep
+    *09)
+      $dry mv "$folder" "${folder}sep"
       ;;
-      *10)
-        $dry mv $folder ${folder}oct
+    *10)
+      $dry mv "$folder" "${folder}oct"
       ;;
-      *11)
-        $dry mv $folder ${folder}nov
+    *11)
+      $dry mv "$folder" "${folder}nov"
       ;;
-      *12)
-        $dry mv $folder ${folder}dec
+    *12)
+      $dry mv "$folder" "${folder}dec"
       ;;
     esac
   done
 }
-args (){
+args() {
   #1 takes args given to script
-  if [[ "$#" == 0 ]] || [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
+  if [[ "$#" == 0 ]] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     precho "automatically organizes content from WhatsApp Images or WhatsApp Video folder"
     precho "it produces a folder structure like: 2019/05/pics/*stuff*"
     precho "--imgs do WhatsApp images (don't use both)"
@@ -151,12 +151,13 @@ args (){
   fi
 }
 # - - -- - - - - - - - - - - - - - - - - - - - - - - -  Code
-args $@
+# HAS UNTESTED SHCHECK CHANGES! ROLL BACK THE COMMIT IF NEEDED
+args "$@"
 [ ! -n "$manual" ] && find_whatsappDir
 renameFolders
-moveFiles $dir $(findYearsWithFiles $dir)
+moveFiles "$dir" "$(findYearsWithFiles "$dir")"
 if [[ -d "$dir/Sent" ]]; then
   dir=$dir/Sent
-  moveFiles $dir $(findYearsWithFiles $dir)
-  $dry trash $dir
+  moveFiles "$dir" "$(findYearsWithFiles "$dir")"
+  $dry trash "$dir"
 fi
