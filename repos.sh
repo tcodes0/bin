0552f81b4e5080c78cc284f9ad5631f087b8e71b
Raphael Thomazella <thomazella9@gmail.com>
2019-09-16 19:28:41 -0300

    chore(misc): rework repos.sh

--- repos.sh
+++ repos.sh
@@ -1,16 +1,13 @@
 #! /usr/bin/env bash
 
 set -e
-GIT_COMMAND="git pull --ff-only"
-
-if [ "$1" == "push" ] || [ "$1" == "p" ]; then
-  GIT_COMMAND="git push"
-fi
 
 cd "$HOME/bin"
 git fetch
-eval "$GIT_COMMAND"
+git push -q
+git pull --ff-only
 
 cd "$HOME/Documents/GoogleDrive/Mackup"
 git fetch
-eval "$GIT_COMMAND"
+git push -q
+git pull --ff-only
